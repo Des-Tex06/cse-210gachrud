aab0d0513e8cbd6531addf354108d5a85b2019bd
Des-Tex06 <clong@byupathway.edu>
2026-03-04 13:41:50 -0600

    Added code to Week 01 Exercise 2.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,50 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine();
+        Console.Write("What is your grade percentage? ");
+        string userGradePercentage = Console.ReadLine();
+        int number = int.Parse(userGradePercentage);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C"; 
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else if (number < 60)
+        {
+            letter = "F";
+        }
+        if (number >= 70)
+        {
+            Console.WriteLine($"Since your grade is {letter},");
+            Console.WriteLine("You have passed the Course!");
+            Console.WriteLine();
+            Console.WriteLine("Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine($"Since your grade is {letter},");
+            Console.WriteLine("You did not pass the course.");
+            Console.WriteLine();
+            Console.WriteLine("Keep trying and pass next time!");
+        }
     }
 }
