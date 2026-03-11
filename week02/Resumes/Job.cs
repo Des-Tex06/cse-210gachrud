24ba651285135dd7859f499361c8bcd25a57642b
Des-Tex06 <clong@byupathway.edu>
2026-03-11 01:51:58 -0500

    Code added to program displaying details including Name, jobtitle, company and year start and year end. Code for Job Class and Resume Class have been added.

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,14 @@
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public Job()
+    {
+    }
+    public int _startYear;
+    public int _endYear; 
+    public void DisplayJobDescription()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
