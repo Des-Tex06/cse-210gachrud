0fd362044e566c9a85f1940085c3931e77fc7170
Des-Tex06 <clong@byupathway.edu>
2026-03-23 19:28:40 -0500

    Code for main program added with instances displaying fraction string along with decimal value for four fractions. File for Fraction class added as well.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fract1 = new Fraction();
+        //Console.WriteLine(fract1.GetTop());
+        //Console.WriteLine(fract1.GetBottom());
+        fract1.GetFractionString();
+        Console.WriteLine(fract1.GetDecimalValue());
+
+        Fraction fract2 = new Fraction(5);
+        //Console.WriteLine(fract2.GetTop());
+        //Console.WriteLine(fract2.GetBottom());
+        fract2.GetFractionString();
+        Console.WriteLine(fract2.GetDecimalValue());
+
+        Fraction fract3 = new Fraction(3,4);
+        //Console.WriteLine(fract3.GetTop()); 
+        //Console.WriteLine(fract3.GetBottom());
+        fract3.GetFractionString();
+        Console.WriteLine(fract3.GetDecimalValue());
+
+        Fraction fract4 = new Fraction(1,3);
+        fract4.GetFractionString();
+        Console.WriteLine(fract4.GetDecimalValue());
+
+
+        
+
     }
 }
