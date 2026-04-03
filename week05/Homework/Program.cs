8593cdb64964386e4cb1a9910e34f245242d2173
Des-Tex06 <clong@byupathway.edu>
2026-04-03 00:53:23 -0500

    Code added to cs files in Homework Folder includling main program and classes.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment studentSummary = new Assignment("Samuel Bennett","Multiplication");
+        Console.WriteLine(studentSummary.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment hwList = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19" );
+        Console.WriteLine(hwList.GetSummary());
+        Console.WriteLine(hwList.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writA = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writA.GetSummary());
+        Console.WriteLine(writA.GetWritingInformation());
     }
 }
